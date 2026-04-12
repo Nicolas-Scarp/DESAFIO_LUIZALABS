7fdee71661b5c55c9c392916b7dfa55e0cb61e9b
Nicolas Scarp <contatonicolasscarp@gmail.com>
2026-04-12 00:32:30 -0300

    feature: creating py files

---
+++ desafios.py
@@ -0,0 +1,16 @@
+import sla
+
+dic = {
+    "1":sla.desafio_1,
+    "2":sla.desafio_2,
+    "3":sla.desafio_2
+}
+
+escolha = input("Qual código quer rodar? ")
+
+func = dic.get(escolha)
+
+if func:
+    func()
+else:
+    print("Função Não Encontrilda !!")
---
+++ sla/__init__.py
@@ -0,0 +1,18 @@
+def desafio_1():
+    entrada = input().strip(" ")
+    preco_str, codigo_promocao = entrada.split()
+
+    preco = float(preco_str)
+
+    if codigo_promocao == "S":
+        preco = preco - (preco * 0.10)
+        print(f"{preco:.2f}")
+    elif codigo_promocao == "N":
+        '''eai, bão?'''
+        print(f"{preco:.2f}")
+    else:
+        print("Código Não Encontrado!")
+
+def desafio_2():
+    print()
+  
